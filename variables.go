8fd8d949b459a76f458444d5a7fffdf85ac55746
xrnurx <nur.rozikin@praweda.id>
2018-12-29 22:30:45 +0700

    update variables

--- variables.go
+++ variables.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	// Deklarasi Variabel Dengan Tipe Data
+	/*/ ################# Deklarasi Variabel Dengan Tipe Data ################# /*/
 	fmt.Println("\n # Deklarasi Variabel Dengan Tipe Data")
 	var a string = "Kopral"
 	var b string
@@ -12,8 +12,8 @@ func main() {
 
 	fmt.Printf("hallo %s %s!\n", a, b)
 
+	/*/	################# Deklarasi Variabel Tanpa Tipe Data ################# /*/
 	fmt.Println("\n # Deklarasi Variabel Tanpa Tipe Data")
-
 	// menggunakan var, tanpa tipe data, menggunakan perantara "="
 	var c = "john"
 
@@ -22,13 +22,14 @@ func main() {
 
 	fmt.Printf("hallo %s %s!\n", c, d)
 
-	//Tanda := hanya digunakan sekali di awal pada saat deklarasi. Untuk assignment nilai selanjutnya harus menggunakan tanda = , contoh:
+	// Tanda := hanya digunakan sekali di awal pada saat deklarasi. Untuk assignment nilai selanjutnya harus menggunakan tanda = , contoh:
 	e := "wick"
 	e = "ethan"
 	e = "bourne"
 
 	fmt.Printf("hallo %s !\n", e)
 
+	/*/ ################# Deklarasi Multi Variabel ################# /*/
 	fmt.Println("\n # Deklarasi Multi Variabel")
 
 	var first, second, third string
@@ -45,6 +46,7 @@ func main() {
 	one, isFriday, twoPointTwo, say := 1, true, 2.2, "hello"
 	fmt.Println("one, isFriday, twoPointTwo, say = ", one, isFriday, twoPointTwo, say)
 
+	/*/ ################# Variabel Underscore _ ################# /*/
 	fmt.Println("\n # Variabel Underscore _")
 	/*
 		Underscore ( _ ) adalah predefined variabel yang bisa dimanfaatkan untuk menampung nilai yang tidak dipakai.
@@ -56,6 +58,7 @@ func main() {
 	name, _ := "john", "wick"
 	fmt.Printf("hallo %s !\n", name)
 
+	/*/ ################# Deklarasi Variabel Menggunakan Keyword 'new' ################# /*/
 	fmt.Println("\n # Deklarasi Variabel Menggunakan Keyword new")
 	//Keyword new digunakan	untuk mencetak data pointer dengan tipe data tertentu.	Nilai data default-nya akan menyesuaikan tipe datanya.
 
